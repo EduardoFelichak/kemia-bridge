169b6cfff74955ca4d31ee4c236944a6f7901cd2
Eduardo Felichak <100370989+EduardoFelichak@users.noreply.github.com>
2024-08-06 18:39:01 -0300

    create address entitie

---
+++ KemiaBridgeApi/KemiaBridge.Domain/Entities/Address.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KemiaBridge.Domain.Entities
+{
+    [Table("address")]
+    public class Address
+    {
+        [Key]
+        public int Id { get; private set; }
+        public string ZipCode { get; private set; }
+        public string Street { get; private set; }
+        public string Complement { get; private set; }
+        public string Neighborhood { get; private set; }
+        public string City { get; private set; }
+        public string State { get; private set; }
+
+        public Address(string zipcode, string street, string complement, string neighborhood, string city, string state) 
+        { 
+            this.ZipCode      = zipcode;
+            this.Street       = street;
+            this.Complement   = complement;
+            this.Neighborhood = neighborhood;
+            this.City         = city;
+            this.State        = state;
+        }
+
+    }
+}
---
+++ KemiaBridgeApi/KemiaBridge.Infra.Data/Repository/Abstract/IAddressRepository.cs
@@ -0,0 +1,12 @@
+﻿using KemiaBridge.Domain.Entities;
+
+namespace KemiaBridge.Infra.Data.Repository.Abstract
+{
+    internal interface IAddressRepository
+    {
+        Task<Address> GetByIdAsync(int id);
+        Task AddAsync(Address address);
+        Task UpdateAsync(Address address);
+        Task DeleteAsync(int id);
+    }
+}
