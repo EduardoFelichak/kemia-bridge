07e3356f67a66f49e1b08a43e5e0096501d7277b
Eduardo Kawan Felichak <eduardo.felichak@questor.local>
2024-08-07 18:48:59 -0300

    pre-configured context

---
+++ KemiaBridgeApi/KemiaBridge.Infra.Data/Context/ConnectionContext.cs
@@ -0,0 +1,17 @@
+﻿using KemiaBridge.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace KemiaBridge.Infra.Data.Context
+{
+    public class ConnectionContext : DbContext
+    {
+        public DbSet<Address> Addresses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseNpgsql(new ConfigurationBuilder()
+                .AddJsonFile("appsettings.local.json", optional: false, reloadOnChange: true)
+                .Build()
+                .GetConnectionString("DefaultConnection"));
+    }
+}
