240c5b35680a6786b40e4d3cb1b39fa5b60bcf63
Eduardo Kawan Felichak <eduardo.felichak@questor.local>
2024-08-07 18:43:25 -0300

    create abstract address repository

---
+++ KemiaBridgeApi/KemiaBridge.Infra.Data/Repository/IAddressRepository.cs
@@ -0,0 +1,13 @@
+﻿using KemiaBridge.Domain.Entities;
+
+namespace KemiaBridge.Infra.Data.Repository
+{
+    public interface IAddressRepository
+    {
+        Task AddAsync(Address address);
+        Task<Address> GetByIdAsync(int id);
+        Task<IEnumerable<Address>> GetAllAsync();
+        Task UpdateAsync(Address address);
+        Task DeleteAsync(int id);
+    }
+}
