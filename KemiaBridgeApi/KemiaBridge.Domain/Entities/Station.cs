8725e673d47cb630624309d10061bd75f39788fa
Eduardo Felichak <100370989+EduardoFelichak@users.noreply.github.com>
2024-08-09 23:59:26 -0300

    add station entitie

---
+++ KemiaBridgeApi/KemiaBridge.Domain/Entities/Station.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KemiaBridge.Domain.Entities
+{
+    [Table("station")]
+    public class Station
+    {
+        [Key]
+        public int StationId { get; private set; }
+        public string Name { get; private set; }
+        public DateTime OperationDate { get; private set; }
+        [ForeignKey(nameof(Person))]
+        public int PersonId { get; private set; }
+        [ForeignKey(nameof(Address))]
+        public int AddressId { get; private set; }
+
+        #pragma warning disable CS8618
+        public Station() { }
+
+        public Station(int stationId, string name, DateTime operationDate, int personId, int addressId)
+        {
+            StationId     = stationId;
+            Name          = name;
+            OperationDate = operationDate;
+            PersonId      = personId;
+            AddressId     = addressId;
+        }
+    }
+}
