e47634a267ed6cb73b015a12f681abf63420c74e
Eduardo Felichak <edukf90@gmail.com>
2024-08-12 08:19:08 -0300

    add station dto

---
+++ KemiaBridgeApi/KemiaBridge.Domain/DTos/StationDto.cs
@@ -0,0 +1,16 @@
+﻿namespace KemiaBridge.Domain.DTos
+{
+    public class StationDto
+    {
+        public int StationId { get; private set; }
+        public string Name { get; set; }
+        public DateTime OperationDate { get; set; }
+        public int PersonId { get; set; }
+        public int AddressId { get; set; }
+
+        public void setNewId(int id)
+        {
+            StationId = id;
+        }
+    }
+}
