9d186584b19490522589ce15fcfaa145b16d8ad3
Eduardo Felichak <edukf90@gmail.com>
2024-08-21 10:44:12 -0300

    add user service

---
+++ KemiaBridgeApi/KemiaBridge.Service/Interface/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using KemiaBridge.Domain.DTos;
+using KemiaBridge.Domain.Entities;
+
+namespace KemiaBridge.Service.Interface
+{
+    public interface IUserService
+    {
+        Task AddAsync(UserDto userDto);
+        Task<IEnumerable<User>> GetAllAsync();
+        Task<User?> GetByIdAsync(int id);
+        Task<User?> SignInAsync(string email, string password);
+        Task UpdateAsync(int id, UserDto userDto);
+        Task DeleteAsync(int id);
+    }
+}
