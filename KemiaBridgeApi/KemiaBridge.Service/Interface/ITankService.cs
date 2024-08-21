bdf411e33475fa3da8fd0ac1176adb211e39e7c3
Eduardo Felichak <edukf90@gmail.com>
2024-08-21 17:23:57 -0300

    add tank service

---
+++ KemiaBridgeApi/KemiaBridge.Service/Interface/ITankService.cs
@@ -0,0 +1,16 @@
+﻿using KemiaBridge.Domain.DTos;
+using KemiaBridge.Domain.Entities;
+using KemiaBridge.Domain.Enums;
+
+namespace KemiaBridge.Service.Interface
+{
+    public interface ITankService
+    {
+        Task AddAsync(TankDto tankDto);
+        Task<IEnumerable<Tank>> GetAllAsync();
+        Task<Tank?> GetByIdAsync(int id);
+        Task<IEnumerable<Tank>> GetByTypeAsync(TankType tankType);
+        Task UpdateAsync(int id, TankDto tankDto);
+        Task DeleteAsync(int id);
+    }
+}
