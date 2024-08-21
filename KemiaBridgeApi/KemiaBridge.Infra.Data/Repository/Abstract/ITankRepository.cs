e7f989054cb3b0b73ad4febb7ddf4d2e9ace1404
Eduardo Felichak <edukf90@gmail.com>
2024-08-21 17:03:32 -0300

    add tank repository

---
+++ KemiaBridgeApi/KemiaBridge.Infra.Data/Repository/Abstract/ITankRepository.cs
@@ -0,0 +1,15 @@
+﻿using KemiaBridge.Domain.Entities;
+using KemiaBridge.Domain.Enums;
+
+namespace KemiaBridge.Infra.Data.Repository.Abstract
+{
+    public interface ITankRepository
+    {
+        Task AddAsync(Tank tank);
+        Task<IEnumerable<Tank>> GetAllAsync();
+        Task<Tank?> GetByIdAsync(int id);
+        Task<IEnumerable<Tank>> GetByTypeAsync(TankType tankType);
+        Task UpdateAsync(Tank tank);
+        Task DeleteAsync(int id);
+    }
+}
