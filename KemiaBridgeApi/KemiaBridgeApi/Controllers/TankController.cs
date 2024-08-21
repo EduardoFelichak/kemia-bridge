99fa9b8f403f14472ebd5f91ab10869db3eb3009
Eduardo Felichak <edukf90@gmail.com>
2024-08-21 17:41:46 -0300

    add tank routes

---
+++ KemiaBridgeApi/KemiaBridgeApi/Controllers/TankController.cs
@@ -0,0 +1,66 @@
+﻿using KemiaBridge.Domain.DTos;
+using KemiaBridge.Domain.Enums;
+using KemiaBridge.Service.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KemiaBridgeApi.Controllers
+{
+    [ApiController]
+    [Route("api/v1/tank")]
+    public class TankController : ControllerBase
+    {
+        private readonly ITankService _tankService;
+
+        public TankController(ITankService tankService)
+        {
+            _tankService = tankService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(TankDto tankDto)
+        {
+            await _tankService.AddAsync( tankDto );
+            return Ok(new
+            {
+                tankId = tankDto.TankId,
+            });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var tanks = await _tankService.GetAllAsync();
+            return Ok(tanks);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var tank = await _tankService.GetByIdAsync( id );
+            if ( tank == null )
+                return NotFound();
+            return Ok(tank);
+        }
+
+        [HttpGet("/type/{type}")]
+        public async Task<IActionResult> GetByType(TankType type)
+        {
+            var tanksByType = await _tankService.GetByTypeAsync( type );
+            return Ok(tanksByType);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, TankDto tankDto)
+        {
+            await _tankService.UpdateAsync( id, tankDto );
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _tankService.DeleteAsync( id );
+            return NoContent();
+        }
+    }
+}
