96f986ea9b61ef874e95daac09e628ce09a94210
Eduardo Felichak <edukf90@gmail.com>
2024-08-22 16:39:58 -0300

    add blower entitie

---
+++ KemiaBridgeApi/KemiaBridge.Domain/Entities/Blower.cs
@@ -0,0 +1,21 @@
+﻿namespace KemiaBridge.Domain.Entities
+{
+    public class Blower
+    {
+        public int BlowerId { get; private set; }
+        public string Tag { get; private set; }
+        public string Description { get; private set; }
+        public int StepId { get; private set; }
+
+        #pragma warning disable CS8618
+        public Blower() { }
+
+        public Blower(int blowerId, string tag, string description, int stepId)
+        {
+            BlowerId    = blowerId;
+            Tag         = tag;
+            Description = description;
+            StepId      = stepId;
+        }
+    }
+}
