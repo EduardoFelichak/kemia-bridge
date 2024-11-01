bb45c1db57e1c61e263b73992c39d67bc568b6d0
Eduardo Felichak <100370989+EduardoFelichak@users.noreply.github.com>
2024-11-01 08:49:26 -0300

    controllers clean

--- KemiaBridgeApi/KemiaBridge.Domain/DTos/StationFormDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace KemiaBridge.Domain.DTos
-{
-    public class StationFormDto
-    {
-        public StationDto Station { get; set; } = null!;
-        public AddressDto Address { get; set; } = null!;
-    }
-}
